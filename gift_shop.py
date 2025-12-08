d73848bf52bfd4c491b8fe6a3e92471decc420fb
pweemaes-ro <zappamotel@quicknet.nl>
2025-12-08 12:12:27 +0100

    Initial Commit Day 2

---
+++ gift_shop.py
@@ -0,0 +1,66 @@
+"""
+Solutions for AoC 2025 Day 2.
+"""
+from collections.abc import Iterator
+from io import TextIOWrapper
+
+
+def _intervals(file: TextIOWrapper) -> Iterator[tuple[int, int]]:
+    """
+    Yield tuple[first, last] from file, where file contains a single line
+    consisting of comma-separated first-last.
+    """
+
+    file.seek(0)
+    for interval in file.readline().split(','):
+        first, last = map(int, interval.split('-'))
+        yield first, last
+
+
+def sum_invalid_ids_01(intervals: Iterator[tuple[int, int]]) -> int:
+    """
+    Return solution for part 1.
+    """
+
+    return sum(sum(x for x in range(first, last + 1)
+                   if not (n := len(s := str(x))) % 2
+                   and s[:n // 2] == s[n // 2:])
+               for first, last in intervals)
+
+
+def sum_invalid_ids_02(intervals: Iterator[tuple[int, int]]) -> int:
+    """
+    Return solution for part 2
+    """
+
+    total = 0
+    for first, last in intervals:
+        for x in range(first, last + 1):
+            for d in range(1, (n := len(s := str(x))) // 2 + 1):
+                if (n % d == 0 and
+                        len(set(s[i: i + d] for i in range(0, n, d))) == 1):
+                    total += x
+                    break
+    return total
+
+
+def _main() -> None:
+    for test in (True, False):
+
+        if test:
+            filename = "gift_shop_test_input_01.txt"
+            expected = 1227775554, 4174379265
+        else:
+            filename = "gift_shop_input_01.txt"
+            expected = 12850231731, 24774350322
+
+        with open(filename, encoding="utf-8") as file:
+            solutions = (sum_invalid_ids_01(_intervals(file)),
+                         sum_invalid_ids_02(_intervals(file)))
+            print(solutions)
+            assert solutions == expected
+
+    print("OK!")
+
+
+_main()
