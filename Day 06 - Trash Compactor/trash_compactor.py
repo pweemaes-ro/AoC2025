280fb1f11743285ab99972b6566fa5f1c9f42c3d
pweemaes-ro <zappamotel@quicknet.nl>
2025-12-14 15:16:54 +0100

    Minor changes.

--- Day 06 - Trash Compactor/trash_compactor.py
+++ Day 06 - Trash Compactor/trash_compactor.py
@@ -10,11 +10,11 @@ nrs_pattern = re.compile(r'\d+')
 ops_pattern = re.compile(r'([*+])')
 
 
-def _transpose(strings: list[str]) -> list[int]:
+def _transpose_to_ints(strings: list[str]) -> list[int]:
     """
-    Convert a list of integer strings to a transposed list of integers. Notice
-    that the input strings may have leading and/or trailing spaces. Return the
-    resulting transposed list of integers.
+    Given a list of strings representing ints transpose the strings and return
+    a list of integers represented by the transposed strings. The input strings
+    may have leading and/or trailing spaces, but must be of equal length!
 
     Example: The list of input strings:
 
@@ -23,15 +23,38 @@ def _transpose(strings: list[str]) -> list[int]:
      "612"
      "996"]
 
-    is transposed to the list of integers:
+    results in transposed strings
 
-    [ 669,
-     4319,
-     626]
+    [" 669",
+     "4319",
+     " 629"]
+
+    so the returned list of integers is
+
+    [669, 4319, 626]
     """
 
     return [int(t) for t in (''.join(s[c] for s in strings)
                              for c in range(len(strings[0])))]
+
+
+def _get_total(ops_and_offsets: list[tuple[str, int]], int_rows: list[str]) \
+        -> int:
+    """
+    Return the total for part 2 calculated from a list of (operator, offset)
+    pairs and a list of string lines from the input.
+    """
+
+    op, offset = ops_and_offsets[0][0], 0
+    total = 0
+
+    for next_op, next_offset in ops_and_offsets[1:]:
+        strings = [int_row[offset: next_offset - 1] for int_row in int_rows]
+        operands = _transpose_to_ints(strings)
+        total += sum(operands) if op == '+' else prod(operands)
+        offset, op = next_offset, next_op
+
+    return total
 
 
 def solve_part_1(file: TextIOWrapper) -> int:
@@ -39,7 +62,7 @@ def solve_part_1(file: TextIOWrapper) -> int:
     Return the solution for part 1.
     """
 
-    nrs = []
+    nr_rows = []
     ops = []
 
     file.seek(0)
@@ -47,19 +70,13 @@ def solve_part_1(file: TextIOWrapper) -> int:
         if line[0] in ('+', '*'):
             ops = ops_pattern.findall(line)
         else:
-            nrs.append(list(map(int, nrs_pattern.findall(line))))
+            nr_rows.append(list(map(int, nrs_pattern.findall(line))))
 
     total = 0
-    for i, op in enumerate(ops):
-        operator = add
-        result = 0
-        if op == '*':
-            operator = mul
-            result = 1
-        for row in nrs:
-            result = operator(result, row[i])
-        total += result
 
+    for i, op in enumerate(ops):
+        operands = [nr_row[i] for nr_row in nr_rows]
+        total += sum(operands) if op == '+' else prod(operands)
     return total
 
 
@@ -74,7 +91,9 @@ def solve_part_2(file: TextIOWrapper) -> int:
     ops_and_offsets = []
 
     for line in file.readlines():
-        if line[0] in ('+', '*'):
+        if line[0] not in ('+', '*'):
+            int_rows.append(line[:-1])  # skip newline char at end of each line
+        else:
             ops_and_offsets = [(match.group(), match.start())
                                for match in ops_pattern.finditer(line)]
             max_length = max(len(s) for s in int_rows)
@@ -82,19 +101,9 @@ def solve_part_2(file: TextIOWrapper) -> int:
             ops_and_offsets.append(('DUMMY', max_length + 1))
             # pad strings in int_rows with spaces if necessary
             int_rows = [s + ' ' * (max_length - len(s)) for s in int_rows]
-        else:
-            int_rows.append(line[:-1])  # skip newline char at end of each line
+            break
 
-    op, offset = ops_and_offsets[0][0], 0
-    total = 0
-
-    for next_op, next_offset in ops_and_offsets[1:]:
-        strings = [int_row[offset: next_offset - 1] for int_row in int_rows]
-        operands = _transpose(strings)
-        total += sum(operands) if op == '+' else prod(operands)
-        offset, op = next_offset, next_op
-
-    return total
+    return _get_total(ops_and_offsets, int_rows)
 
 
 def _main() -> None:
